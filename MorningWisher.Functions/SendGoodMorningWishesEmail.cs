9b8d6d3a2246a146834e9b476cf619eb900bc3a5
yamesant <yamesant.try.and.trash@gmail.com>
2024-05-01 14:54:23 +1000

    Add emailing logic

--- MorningWisher.Functions/SendGoodMorningWishesEmail.cs
+++ MorningWisher.Functions/SendGoodMorningWishesEmail.cs
@@ -1,3 +1,5 @@
+using Azure;
+using Azure.Communication.Email;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Options;
 
@@ -13,6 +15,21 @@ public class SendGoodMorningWishesEmail
     [Function("SendGoodMorningWishesEmail")]
     public async Task Run([TimerTrigger("0 0 22 * * *", RunOnStartup = true)] TimerInfo myTimer)
     {
-        string recipientAddress = _emailCommunicationConfig.RecipientAddress;
+        var emailClient = new EmailClient(_emailCommunicationConfig.ConnectionString);
+        EmailSendOperation emailSendOperation = await emailClient.SendAsync(
+            WaitUntil.Completed,
+            senderAddress: _emailCommunicationConfig.SenderAddress,
+            recipientAddress: _emailCommunicationConfig.RecipientAddress,
+            subject: "Good Morning!",
+            htmlContent:
+            "<html><body>" +
+            "Open your eyes, see the morning light, <br><br>" +
+            "Each day is a chance to reach new height. <br><br>" +
+            "Keep going strong, don't ever dismay, <br><br>" +
+            "You'll conquer your dreams, come what may! <br><br>" +
+            "Lots of love, <br>" +
+            "Yamesant" +
+            "</body></html>"
+        );
     }
 }
